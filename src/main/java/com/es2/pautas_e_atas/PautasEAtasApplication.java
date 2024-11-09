b68d8db2d9967acecc75e4a60d577dc7722a78ed
DiegoGM55 <diego.mota@unesp.br>
2024-11-09 14:14:16 -0300

    feat: first commit

---
+++ src/main/java/com/es2/pautas_e_atas/PautasEAtasApplication.java
@@ -0,0 +1,13 @@
+package com.es2.pautas_e_atas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautasEAtasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautasEAtasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es2/pautas_e_atas/PautasEAtasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.pautas_e_atas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautasEAtasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
