ed6fd7be016318ccba794a8fa2c43ae61c160f1e
almeidaAugusto <augusto.mds@outlook.com>
2024-11-13 13:56:30 -0300

    token de autenticacao e login

---
+++ src/main/java/com/es2/pautas_e_atas/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.es2.pautas_e_atas.controller;
+
+import com.es2.pautas_e_atas.config.TokenService;
+import com.es2.pautas_e_atas.domain.Usuario.AuthenticationDTO;
+import com.es2.pautas_e_atas.domain.Usuario.LoginResponseDTO;
+import com.es2.pautas_e_atas.domain.Usuario.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+
+        return  ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
---
+++ src/main/java/com/es2/pautas_e_atas/domain/Usuario/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.es2.pautas_e_atas.domain.Usuario;
+
+public record AuthenticationDTO(String email, String senha) {
+}
